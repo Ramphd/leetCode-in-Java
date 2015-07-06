c95c4de81fdc26987254e7ed83f9766a5562d57b
Ramphd <wadqse@126.com>
2015-07-06 13:47:01 +0800

    #add a new method to Quiz3

--- LeetCode/src/algorithms/LongestSubstring.java
+++ LeetCode/src/algorithms/LongestSubstring.java
@@ -12,8 +12,20 @@ import java.util.List;
  * which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
  */
 public class LongestSubstring {
-
-	public static int lengthOfLongestSubstring(String s) {
+	public static int lengthOfLongestSubstring1(String s) {//利用charASCII 码的数字和第一题有一样的取巧的思想
+        boolean[] occur = new boolean[127];
+        int result = 0;
+        for (int i = 0, j = 0; j < s.length(); i++) {
+            for (; j < s.length() && !occur[s.charAt(j)]; j++) {
+                occur[s.charAt(j)] = true;
+            }
+            result = Math.max(result, j - i);
+            occur[s.charAt(i)] = false;
+        }
+        return result;
+    }
+	
+public static int lengthOfLongestSubstring(String s) {
         String ss = s;
         if(ss.equals("")){
         	return 0;
@@ -41,7 +53,10 @@ public class LongestSubstring {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s = "abcdefashjuib";
-		System.out.println(lengthOfLongestSubstring(s));
+		System.out.println(lengthOfLongestSubstring1("ssss"));
+		int[] a = new int[128];
+		a[97] = 33;
+		System.out.println(a['a']);
 	}
 
 }
