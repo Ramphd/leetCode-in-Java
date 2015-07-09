c1c1ee99d443f2fc790447ea200b98c93fbb05e1
Ramphd <wadqse@126.com>
2015-07-09 13:34:54 +0800

    # Q5

--- LeetCode/src/algorithms/LongestPalindromicSubstring.java
+++ LeetCode/src/algorithms/LongestPalindromicSubstring.java
@@ -13,8 +13,8 @@ public  static String longestPalindromeWithDP(String s) {
 	int length = s.length();
 	if(s.length() < 2)
 		return s;
-	char[] c = new char[length];
-	s.getChars(0, length, c, 0);
+	char[] c = s.toCharArray();
+//	s.getChars(0, length, c, 0);
 	//System.out.println(c[1]);
     boolean table[][] = new boolean[1000][1000];    
     int paliStart = 0;
@@ -41,9 +41,45 @@ public  static String longestPalindromeWithDP(String s) {
     }
 	return s.substring(paliStart,paliStart+ maxLength);
     }
-	public static void main(String[] args) {
+
+public static String withCenterExpand(String s){
+	int length = s.length();
+	if(length < 2) return s;
+	char c[] = new char[length];
+	c = s.toCharArray();
+	String sub = "";
+	for(int i = 0 ;i < length ;i++){
+		String sub1 = centerE(c, i, i);
+		String sub2 = centerE(c, i, i+1);
+		//System.out.println(sub.length()+" " + sub1 +" "+ sub2);
+		if(sub1.length() > sub2.length()){
+			if(sub1.length() > sub.length()){
+				sub = sub1;
+			}
+		}else{
+			if(sub2.length() > sub.length())
+				sub = sub2;
+		}
+			
+	}
+	
+	return sub;
+}
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(longestPalindromeWithDP("a"));
+		System.out.println(longestPalindromeWithDP("aaa"));
+		System.out.println(withCenterExpand("bb"));
 	}
 
+public static String centerE(char[] c ,int x ,int y){
+	int l = x;
+	int r = y;
+	int n = c.length;
+	
+	while (l >=0 && r<=n-1 && c[l] == c[r]){
+		l--;
+		r++;
+	}
+	return String.copyValueOf(c, l+1, r-l-1);
+}
 }
